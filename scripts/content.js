57803303f891202edc7ffd93bbddde63deaf4343
Omar De Los Santos <omar@xolos.tech>
2025-10-26 19:19:02 -0700

    Toggle pencil mode with 'p'

--- scripts/content.js
+++ scripts/content.js
@@ -1,46 +1,23 @@
-function renderReadingTime(article) {
-  // If we weren't provided an article, we don't need to render anything.
-  if (!article) {
-    return;
+function togglePencilMode() {
+  const inactiveIcon = document.querySelector('.xwd__toolbar_icon--pencil');
+  const inactivePencilModeButton = inactiveIcon?.closest('button');
+  if (inactivePencilModeButton) {
+    inactivePencilModeButton.click();
   }
 
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+  const activeIcon = document.querySelector('.xwd__toolbar_icon--pencil-active');
+  const activePencilModeButton = activeIcon?.closest('button');
+  if (activePencilModeButton) {
+    activePencilModeButton.click();
+  }
 }
 
-renderReadingTime(document.querySelector("article"));
-
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    // If a new article was added.
-    for (const node of mutation.addedNodes) {
-      if (node instanceof Element && node.tagName === 'ARTICLE') {
-        // Render the reading time for this particular article.
-        renderReadingTime(node);
-      }
+function addEventListeners() {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "p") {
+      togglePencilMode();
     }
-  }
-});
+  });
+}
 
-// https://developer.chrome.com/ is a SPA (Single Page Application) so can
-// update the address bar and render new content without reloading. Our content
-// script won't be reinjected when this happens, so we need to watch for
-// changes to the content.
-observer.observe(document.querySelector('devsite-content'), {
-  childList: true
-});
+addEventListeners();
